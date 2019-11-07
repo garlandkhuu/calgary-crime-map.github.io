1d64604e088fc3b3a8e2a48a73b37b5eb1f80ab6
JasonDam07 <55894141+JasonDam07@users.noreply.github.com>
2019-11-07 15:42:47 -0700

    Added some angular

--- testFunc.js
+++ testFunc.js
@@ -1,3 +1,12 @@
 function changeTxt() {
     document.getElementById('about').innerHTML = 'Changed';
 }
+
+var app = angular.module('knowledge'. []);
+app.controller('infoCtrl', function($scope){
+    $scope.firstName = "Jason";
+    $scope.lastName = "Dam";
+    $scope.display = function() {
+        return $scope.feedBck;
+    }
+})
