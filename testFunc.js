c01d8f19b51be34a2e5f56d9da491b84ddd60eb8
JasonDam07 <55894141+JasonDam07@users.noreply.github.com>
2019-11-07 16:11:10 -0700

    Update

--- testFunc.js
+++ testFunc.js
@@ -6,7 +6,11 @@ var app = angular.module('knowledge', []);
 app.controller('infoCtrl', function($scope){
     $scope.firstName = "Jason";
     $scope.lastName = "Dam";
-    $scope.display = function() {
-        return $scope.feedBck;
+    $scope.display = function(input) {
+        var txt;
+        if (input == 'first name'){
+            txt = firstName;
+        }
+        return $scope.txt;
     }
 })
